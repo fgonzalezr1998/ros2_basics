cee3c2bfc149f11be4fbaa3e55d0665d6d80bfb7
fgonzalezr1998 <fergonzaramos@yahoo.es>
2020-12-02 13:59:54 +0100

    [octomaps_comparator] changes added

--- octomaps_comparator/src/comparator.cpp
+++ octomaps_comparator/src/comparator.cpp
@@ -1,28 +1,79 @@
-#include <rclcpp/rclcpp.hpp>
 #include <string>
+#include <rclcpp/rclcpp.hpp>
+#include <octomap_msgs/msg/octomap.hpp>
+
+using std::placeholders::_1;
 
 class Comparator : public rclcpp::Node
 {
 public:
-
-    Comparator(const std::string node_name)
+    Comparator(const std::string & node_name)
     :
     Node(node_name)
+    {
+        initParams();
+
+        octomap_kdtree_sub_ = this->create_subscription<octomap_msgs::msg::Octomap>(
+            octomap_kdtee_topic_,
+            rclcpp::QoS(1).best_effort().durability_volatile(),
+            std::bind(&Comparator::octomap_kdtreeCb, this, std::placeholders::_1));
+
+        octomap_erosion_sub_ = this->create_subscription<octomap_msgs::msg::Octomap>(
+            octomap_erosion_topic_,
+            rclcpp::QoS(1).best_effort().durability_volatile(),
+            std::bind(&Comparator::octomap_erosionCb, this, std::placeholders::_1));
+    }
+
+    void
+    step()
     {
         ;
     }
 
 private:
+    void
+    octomap_kdtreeCb(const octomap_msgs::msg::Octomap::SharedPtr msg)
+    {
+        kdtree_octree_ = msg;
+    }
+
+    void
+    octomap_erosionCb(const octomap_msgs::msg::Octomap::SharedPtr msg)
+    {
+        erosion_octree_ = msg;
+    }
+
+    void
+    initParams()
+    {
+        this->declare_parameter("octomap_kdtree_topic", "octomap_kdtree");
+        this->declare_parameter("octomap_erosion_topic", "octomap_erosion");
+
+        this->get_parameter("octomap_kdtree_topic", octomap_kdtee_topic_);
+        this->get_parameter("octomap_erosion_topic", octomap_erosion_topic_);
+    }
+
+    rclcpp::Subscription<octomap_msgs::msg::Octomap>::SharedPtr octomap_kdtree_sub_,
+        octomap_erosion_sub_;
+
+    octomap_msgs::msg::Octomap::SharedPtr kdtree_octree_, erosion_octree_;
+
+    std::string octomap_kdtee_topic_, octomap_erosion_topic_;
 };
 
 int
 main(int argc, char ** argv)
 {
-    Comparator::SharedPtr comparator_node;
-
     rclcpp::init(argc, argv);
 
-    comparator_node = std::make_shared<Comparator>("octomaps_comparator_node");
+    auto comparator_node = std::make_shared<Comparator>("octomaps_comparator_node");
+
+    rclcpp::Rate loop_rate(10);
+    while(rclcpp::ok()) {
+        comparator_node->step();
+        rclcpp::spin_some(comparator_node);
+        loop_rate.sleep();
+    }
 
     exit(EXIT_SUCCESS);
 }
